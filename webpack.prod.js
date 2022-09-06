e3661422f8b12b28bce037e9d183f0aff5e306ec
Steven Zuluaga <steven000069@hotmail.com>
2022-09-06 00:48:08 -0500

    Webpack Production

---
+++ webpack.prod.js
@@ -0,0 +1,44 @@
+const HtmlWebpack = require('html-webpack-plugin')
+const MiniCssExtract = require('mini-css-extract-plugin')
+
+module.exports = {
+     mode: 'production',
+     output: {
+          clean: true
+     },
+     module: {
+          rules: [
+               {
+                    //Realizar expresiones reguales
+                    test: /\.html$/,
+                    loader: 'html-loader',
+                    options: {
+                         sources: false
+                    }
+               },
+               {
+                    test: /\.css$/,
+                    exclude: /style.css$/,
+                    use: ['style-loader', 'css-loader']
+               },
+               {
+                    test: /style.css$/,
+                    use: [MiniCssExtract.loader, 'css-loader']
+               },
+               {
+                    test: /\.(png|jpe?g|gif)$/,
+                    loader: 'file-loader'
+               }
+          ]
+     },
+     plugins: [
+          new HtmlWebpack({
+               template: './index.html',
+               //filename:'main.html'
+          }),
+          new MiniCssExtract({
+               filename: '[name][fullhash].css',
+               ignoreOrder: false
+          })
+     ]
+}
